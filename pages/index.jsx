c5d02ea51475eb20a7f7d80cb3acbacfb95a4747
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-01-16 19:07:35 +0400

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,24 @@
+
+import { Input } from '../components/Input';
+import { LikeButton as LikeButton1 } from '../components/LikeButton_class';
+import { LikeButton } from '../components/LikeButton_func';
+import { StateDiff } from '../components/StateDiff';
+
+
+export default function HomePage() {
+  return <>
+    <StateDiff />
+    <Input />
+    <hr />
+    <h1>Hello, Next.js!</h1>
+    <hr />
+    class:
+    <LikeButton1 color="green" start="100" />
+    <LikeButton1 big step="9" start={99} />
+    <hr />
+    funct:
+    <LikeButton />
+    <LikeButton color="green" start="100" />
+    <LikeButton big step="9" start={99} border />
+  </>;
+}
