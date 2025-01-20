78e961ce4d244ca288f6cd2aa9382f57185e15e9
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2025-01-20 19:01:17 +0400

    до useEffect

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
 
-export function Header(){
+const pages = [
+  { href: '/', title: 'Home' },
+  { href: '/about', title: 'About' },
+  { href: '/user-list', title: 'User List (jsph)' },
+];
+
+export function Header() {
   return <header>
     <nav>
       <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
+        {pages.map(({href,title})=>
+        <li key={href}>
+          <Link href={href}>{title}</Link>
+        </li>)
+        }
       </ul>
     </nav>
   </header>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,13 @@
+import '../global.css';
+
 import { Header } from '../components/Header';
 
 export default function App({ Component, pageProps }) {
   return <>
     <Header />
-    <Component {...pageProps} />
-    <hr />
+    <main>
+      <Component {...pageProps} />
+    </main>
     <footer>
       (c)2025 Я
     </footer>
